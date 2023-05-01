7d86e171ab821b3b49f7c5fd81ae478a4fb06e76
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-05-01 18:25:43 +0300

    Preview & Setup

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <main>
+    <v-container>
+      <v-sheet
+        class="d-flex justify-center flex-wrap text-center mx-auto py-6"
+        elevation="4"
+        height="250"
+        rounded
+        max-width="800"
+        width="100%"
+      >
+        <div>
+          <h2 class="text-h4 font-weight-black text-orange">Welcome!</h2>
+        </div>
+      </v-sheet>
+    </v-container>
+  </main>
+</template>

134f21caae0a50f0c5043967ef6fd16b4139502c
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-05-04 20:43:02 +0300

    Switching Between Forms

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,6 +1,9 @@
 <script setup>
+import { ref } from 'vue'
 import SignupForm from '@/components/SignupForm.vue'
 import LoginForm from '@/components/LoginForm.vue'
+
+const showLogin = ref(true)
 </script>
 
 <template>
@@ -8,9 +11,26 @@ import LoginForm from '@/components/LoginForm.vue'
     <v-container>
       <v-row justify="center" no-gutters>
         <v-sheet class="w-100 d-flex justify-center flex-wrap mx-auto py-6" max-width="480" rounded>
-          <h1 class="my-4 text-h5 text-uppercase">Добро пожаловать</h1>
-          <!-- <SignupForm /> -->
-          <LoginForm />
+          <template v-if="showLogin">
+            <h1 class="my-4 text-h5 text-uppercase">Вход</h1>
+            <LoginForm />
+            <div>
+              Еще нет аккаунта?
+              <span class="font-weight-bold text-decoration-underline" @click="showLogin = false"
+                >Регистрация</span
+              >
+            </div>
+          </template>
+          <template v-else>
+            <h1 class="my-4 text-h5 text-uppercase">Регистрация</h1>
+            <SignupForm />
+            <div>
+              Уже зарегистрировался?
+              <span class="font-weight-bold text-decoration-underline" @click="showLogin = true"
+                >Войти</span
+              >
+            </div>
+          </template>
         </v-sheet>
       </v-row>
     </v-container>

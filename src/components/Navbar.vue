547d165193ec8a60579f30af344449af02c0de6f
Alexander Bulanov <alex-bulanov.mail@yandex.ru>
2023-05-06 15:03:44 +0300

    Getting the Current User (Composable)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref } from 'vue'
-
+import getUser from '@/composables/getUser'
 import useLogout from '@/composables/useLogout'
 
 const { error, logout } = useLogout()
+const { user } = getUser()
 
 async function handleLogout(params) {
   await logout()
@@ -15,8 +15,15 @@ async function handleLogout(params) {
 </script>
 
 <template>
-  <v-app-bar>
-    <v-toolbar-title>Navbar</v-toolbar-title>
+  <v-app-bar v-if="user">
+    <v-toolbar-title>
+      <p class="text-body-1">
+        Hey, <span class="font-weight-bold">{{ user.displayName }}</span>
+      </p>
+      <p class="text-body-2">
+        Currently logged is as <span class="font-weight-bold">{{ user.email }}</span>
+      </p>
+    </v-toolbar-title>
     <v-btn prepend-icon="mdi-logout" @click="handleLogout">Logout</v-btn>
   </v-app-bar>
 </template>
